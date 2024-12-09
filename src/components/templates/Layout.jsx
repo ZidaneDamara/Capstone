9c95f862b072db4d7fed2bed58163a376baad2b8
ZidaneDamara <zidanedamara21@gmail.com>
2024-12-09 22:12:17 +0700

    delete container

--- src/components/templates/Layout.jsx
+++ src/components/templates/Layout.jsx
@@ -10,9 +10,7 @@ export default function Layout() {
     <div className="min-h-screen bg-gray-50 flex flex-col">
       <Header />
       {!isLoginPage && <CategoryNav />}
-      <main className="flex-grow mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-8">
-        <Outlet />
-      </main>
+      <Outlet />
     </div>
   );
 }
